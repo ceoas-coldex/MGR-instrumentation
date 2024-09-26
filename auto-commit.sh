76980b3cc1251c38310ea96c19ee3eb68ae16623
Ali Jones <aliclara564@gmail.com>
2024-09-26 16:57:27 -0700

    Delete auto-commit.sh

--- auto-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd C:\\Users\\ceoas_coldex\\Documents\\GitHub\\MGR-instrumentation
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -m "Regular auto-commit $(timestamp)"
-git push origin main
