found the bug that was making Interpreter fail silently

main_pipeline/interpreter.py
@@ -98,7 +98,7 @@ class Interpreter():
         # print(f"time difference 3: {self.big_data["Abakus Particle Counter"]["Time (epoch)"] - self.big_data["Flowmeter"]["Time (epoch)"]}")
         # # print(f"time difference 4: {self.big_data["Abakus Particle Counter"]["Time (epoch)"] - self.big_data["Picarro Water"]["Time (epoch)"]}")
         
-        # print(self.big_data)
+        print(self.big_data)
         
         # Write to the output bus
         output_bus.write(copy.deepcopy(self.big_data))
@@ -108,6 +108,8 @@ class Interpreter():
             channels = list(self.big_data[name]["Data"].keys())
             for channel in channels:
                 self.big_data[name]["Data"][channel] = np.nan
+
+        # print(self.big_data)
 
 
     ## ------------------- ABAKUS PARTICLE COUNTER ------------------- ##
@@ -366,10 +368,8 @@ class Interpreter():
                 logger.warning(f"Error in extracting time and data from picarro reading: {e}. Probably not a tuple. Not updating measurement")
             # If it succeeded, process the data
             else:
-                if data_out == "nan":
-                    self.big_data["Picarro Gas"]["Time (epoch)"] = timestamp
-                    return
                 try:
+                    self.big_data["Picarro Gas"]["Time (epoch)"] = timestamp
                     # data_out[0] # the time at which the measurement was sampled, probably different than timestamp because
                     # the computer clocks drift
                     self.big_data["Picarro Gas"]["Time (epoch)"] = timestamp
@@ -398,7 +398,7 @@ class Interpreter():
         try:
             timestamp, (fsetpoint, meas, fmeas_and_temp) = bronkhorst_data
         # If that didn't work, log it
-        except KeyError as e:
+        except TypeError as e:
             logger.warning(f"Error in extracting time and data from bronkhorst reading: {e}. Probably not a tuple. Not updating measurement")
         # If it did work, parse the data
         else:
@@ -421,7 +421,6 @@ class Interpreter():
                 self.big_data["Bronkhorst Pressure"]["Data"]["Measurement (%)"] = measure
                 self.big_data["Bronkhorst Pressure"]["Data"]["Measurement (mbar a)"] = fmeasure
                 self.big_data["Bronkhorst Pressure"]["Data"]["Temperature (C)"] = temp
-                
             except KeyError as e:
                 logger.warning(f"Error in saving bronkhorst data to big dictionary: No key {e}. Not updating measurement")
             except Exception as e:

sensor_interfaces/sim_instruments.py
@@ -93,7 +93,6 @@ class Picarro():
     def initialize_pyserial(self, port, baud):
         logger.info(f"Fake hardware, pretending to use serial port {port} with baud {baud}")
 
-    
     @log_on_start(logging.INFO, "Initializing Picarro", logger=logger)
     def initialize_picarro(self):
         """
@@ -257,10 +256,12 @@ class Bronkhorst():
         timestamp = time.time()
 
         # If we're in debug mode, return this fake reading
-        if debug:
-            output = (fsetpoint, meas, fmeas_and_temp)
-        # Otherwise, return NAN
-        else:
-            output = ("nan", "nan")
+        # if debug:
+        #     output = (fsetpoint, meas, fmeas_and_temp)
+        # # Otherwise, return NAN
+        # else:
+        #     output = ("nan", "nan")
+
+        output = np.nan
         
         return timestamp, output